085454d5de37fb9b856a4bb1fbc9321211356977
Apocalo <rbedoyaco@gmail.com>
2025-03-13 18:56:03 -0500

    Update all packages

--- Application/Customers/Commands/CreateCustomer/CreateCustomerCommandValidator.cs
+++ Application/Customers/Commands/CreateCustomer/CreateCustomerCommandValidator.cs
@@ -8,7 +8,7 @@ namespace Application.Customers.Commands.CreateCustomer
         {
             RuleFor(r => r.Name).NotEmpty().MaximumLength(22);
             RuleFor(r => r.LastName).NotEmpty().MaximumLength(22);
-            RuleFor(r => r.Email).NotEmpty().EmailAddress();
+            RuleFor(r => r.Email).NotEmpty().EmailAddress().MaximumLength(250);
             RuleFor(r => r.Country).NotEmpty().MaximumLength(24);
             RuleFor(r => r.Department).NotEmpty().MaximumLength(24);
             RuleFor(r => r.City).NotEmpty().MaximumLength(24);
